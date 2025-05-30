dd6f5708529b15ea51b4b713f1321d962dde24a1
Anil Thakor <anilthakor7007@gmail.com>
2025-05-30 17:03:13 +0530

    added new projects in portfolio

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -16,12 +16,12 @@ export const Banner = () => {
       <Container className="">
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my portfolio</span>
-            <h1>{`Hi! I'm Anil Thakor`} </h1>
-            <p>
-              Motivated Full stack Developer with foundational knowledge in HTML, CSS,
-              Tailwind CSS, Bootstrap, JavaScript, TypeScript, React.js,
-              Next.js, Node.js, Express.js, and MongoDB. Enthusiastic about
+            <span className="tagline" style={{ color: "#fff" }}>Welcome to my portfolio</span>
+            <h1 style={{ color: "#fff" }}>{`Hi! I'm Anil Thakor`} </h1>
+            <p style={{ color: "#e8e9eb" }}>
+              Motivated Full stack Developer with foundational knowledge in{" "}
+              <b>HTML</b>, <b>CSS</b>, <b>Tailwind CSS</b>, <b>Bootstrap</b>, <b>JavaScript</b>, <b>TypeScript</b>,{" "}
+              <b>React.js</b>, <b>Next.js</b>, <b>Node.js</b>, <b>Express.js</b>, and <b>MongoDB</b>. Enthusiastic about
               building responsive, user-friendly web applications. Eager to
               apply modern web development practices and collaborate with
               dynamic teams to drive project success.
